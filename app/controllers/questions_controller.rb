21aaaba072a1079c5244d181b2e06c479dbcacb7
Ali-Hamza Asam <asamalihamza@gmail.com>
2022-11-14 15:16:36 +0000

    Done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @result = nil
+    if @question == "I am going to work"
+      @result = "Great!"
+    elsif @question.end_with?("?")
+      @result = "Silly question, get dressed and go to work!"
+    else
+      @result = "I don't care, get dressed and go to work!"
+    end
+  end
+end
